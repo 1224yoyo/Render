1012f7a9728b007c478eaa0e20687982af01be0b
yoyo10231224 <xuyousheng27@gmail.com>
2026-03-27 21:29:22 +0800

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,34 @@
+#flask.py
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # 1. 匯入它
+import redis
+import json
+
+app = Flask(__name__)
+
+# 直接貼上你的連線字串
+url = "rediss://default:AZrhAAIncDFkYjBhY2ZhMmE5NTY0ODdhOTJhOGNkYTliNDRiMjIxOXAxMzk2NDk@guiding-weevil-39649.upstash.io:6379"
+
+# 連線 (rediss 代表使用 TLS 加密)
+r = redis.from_url(url, decode_responses=True)
+
+CORS(app)
+
+@app.route("/")
+def read_user():
+    
+    name = request.args.get("name")
+    password = request.args.get("password")
+    js={"data":None}
+    data = r.json().get("user_json", f"$.{name}")
+    if data!=[]:
+        data=data[0]
+        if password==data["password"]:
+            js["data"]= f'name: {data["name"]} password: {data["password"]}'
+        else:js["data"]="password no True"
+    else:js["data"]= "no find User"
+    return jsonify(js)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+#flask.py
