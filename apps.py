a6953ba01c13d168b06cbb04417d5215d8eb66f3
yoyo10231224 <xuyousheng27@gmail.com>
2026-03-28 22:14:55 +0800

    Update apps.py

--- apps.py
+++ apps.py
@@ -16,16 +16,22 @@ CORS(app)
 
 @app.route("/login", methods=["POST"])
 def read_user():
-    name = request.args.get("name")
-    password = request.args.get("password")
-    js={"data":None}
+    name = request.form.get("username")   # 🔥 改這裡
+    password = request.form.get("password")
+
+    js = {"data": None}
+
     data = r.json().get("user_json", f"$.{name}")
-    if data!=[]:
-        data=data[0]
-        if password==data["password"]:
-            js["data"]= f'name: {data["name"]} password: {data["password"]}'
-        else:js["data"]="password no True"
-    else:js["data"]= "no find User"
+
+    if data != []:
+        data = data[0]
+        if password == data["password"]:
+            js["data"] = f'name: {data["name"]} password: {data["password"]}'
+        else:
+            js["data"] = "password no True"
+    else:
+        js["data"] = "no find User"
+
     return jsonify(js)
 
 if __name__ == "__main__":
